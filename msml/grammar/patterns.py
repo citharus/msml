8023ea56ca941162070d5e6d8a94cc11d1bb3cfb
citharus <citharus@protonmail.com>
2021-08-28 12:01:13 +0200

    :sparkles: Added `HEADING_3` pattern

--- msml/grammar/patterns.py
+++ msml/grammar/patterns.py
@@ -22,7 +22,7 @@
 
 import re
 
-__all__: list[str] = ['HEADING_1', 'HEADING_2']
+__all__: list[str] = ['HEADING_1', 'HEADING_2', 'HEADING_3']
 
 HEADING_1: re.Pattern = re.compile(
     rf'(?:#h1\s*)(?P<text>[\w\.]+(?: +[\w\.]+)*)',
@@ -30,5 +30,9 @@ HEADING_1: re.Pattern = re.compile(
 )
 HEADING_2: re.Pattern = re.compile(
     rf'(?:#h2\s*)(?P<text>[\w\.]+(?: +[\w\.]+)*)',
+    re.IGNORECASE,
+)
+HEADING_3: re.Pattern = re.compile(
+    rf'(?:#h3\s*)(?P<text>[\w\.]+(?: +[\w\.]+)*)',
     re.IGNORECASE,
 )
