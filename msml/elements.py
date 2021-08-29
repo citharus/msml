96f4e1af256063ad45732b9c6afa0af370dbedc4
citharus <citharus@protonmail.com>
2021-08-29 19:42:18 +0200

    :truck: Renamed `page_title` to `page_heading`

--- msml/elements.py
+++ msml/elements.py
@@ -93,7 +93,7 @@ HTML: str = """<!DOCTYPE html>
 <script async src="https://cdn.jsdelivr.net/npm/mathjax@3.1/es5/startup.js"></script>
 </head>
 <body>
-{page_title}
+{page_heading}
 {sections}
 </body>
 </html"""
